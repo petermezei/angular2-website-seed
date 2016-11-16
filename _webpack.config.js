e964d59b5e9eb83b7ad107fbf6bb0ba3bf416cf3
Peter Mezei <peter.mezei@slamby.com>
2016-11-16 17:21:16 +0100

    package updated

--- _webpack.config.js
+++ _webpack.config.js
@@ -2,25 +2,18 @@ var sliceArgs = Function.prototype.call.bind(Array.prototype.slice);
 var toString  = Function.prototype.call.bind(Object.prototype.toString);
 var NODE_ENV  = process.env.NODE_ENV || 'development';
 var pkg = require('./package.json');
-
-// Polyfill
 Object.assign = require('object-assign');
-
-// Node
 var path = require('path');
-
-// NPM
 var webpack = require('webpack');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-
-// Webpack Plugins
 var OccurenceOrderPlugin = webpack.optimize.OccurenceOrderPlugin;
 var CommonsChunkPlugin   = webpack.optimize.CommonsChunkPlugin;
 var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var DedupePlugin   = webpack.optimize.DedupePlugin;
 var DefinePlugin   = webpack.DefinePlugin;
 var BannerPlugin   = webpack.BannerPlugin;
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 /*
  * Config
@@ -36,16 +29,6 @@ module.exports = {
     reasons: true
   },
 
-  // our Development Server config
-  devServer: {
-    inline: true,
-    colors: true,
-    historyApiFallback: true,
-    contentBase: '/src',
-    publicPath: '/src/build'
-  },
-
-  //
   entry: {
     'bundle': [
       'es6-shim',
@@ -59,7 +42,7 @@ module.exports = {
 
   // Config for our build files
   output: {
-    path: path.join(__dirname, '/src/build/'),
+    path: __dirname + '/src/build/',
     filename: '[name].js',
     sourceMapFilename: '[name].js.map',
     chunkFilename: '[id].chunk.js',
@@ -73,47 +56,37 @@ module.exports = {
 
   module: {
     loaders: [
-      // Support for .ts files.
       {
-          test: /\.ts$/,
-          loader: 'ts',
-          exclude: [/node_modules/]
+        test: /\.ts$/,
+        loader: 'ts',
+        exclude: [/node_modules/]
       },
-      
-      // Support for *.json files.
-      { test: /\.json$/,  loader: 'json' },
-
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
       {
         test: /\.scss$/,
-        loaders: ['exports-loader?module.exports.toString()', 'css', 'sass', "sass-resources"]
+        loaders: ['sass', 'sass-resources']
       },
-
       {
         test: /\.(png|jpg|jpeg|gif|ico)(\?[a-z0-9]+)?$/,
         loaders: [
           'file?name=images/[name].[hash].[ext]'
         ]
       },
-
-      // support for .html as raw text
-      { test: /\.(html|css)$/,  loader: 'raw' },
-    ],
-    noParse: [
-      /rtts_assert\/src\/rtts_assert/,
-      /reflect-metadata/
+      {
+        test: /\.(html|css)$/,
+        loader: 'raw'
+      }
     ]
   },
-
   // Or array of paths
   sassResources: [
-    './node_modules/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss',
-    './node_modules/bootstrap-sass/assets/stylesheets/bootstrap/mixins/*.scss',
-    './src/assets/_common.scss'
+    'node_modules/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss',
+    'node_modules/bootstrap-sass/assets/stylesheets/bootstrap/mixins/*.scss',
+    'src/assets/_common.scss'
   ],
-
-  ts: {
-    silent: true
-  },
 
   plugins: [
     new DefinePlugin({
@@ -134,14 +107,5 @@ module.exports = {
     {
       reload: true
     })
-  ],
-
-  /*
-   * When using `templateUrl` and `styleUrls` please use `__filename`
-   * rather than `module.id` for `moduleId` in `@View`
-   */
-  node: {
-    crypto: false,
-    __filename: true
-  }
+  ]
 };
