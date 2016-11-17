60d82962ffd2276d190394a1fb43c198fae6cda9
Peter Mezei <peter.mezei@slamby.com>
2016-11-17 14:05:05 +0100

    content v1

--- src/app/pricing/pricing.model.ts
+++ src/app/pricing/pricing.model.ts
@@ -1,6 +1,13 @@
 export class Price {
     constructor(
         public name: string,
-        public price: string
+        public tier: string,
+        public core: number,
+        public memory: number,
+        public storage: number,
+        public wholesale: number,
+        public technicalFee: number,
+        public slambyLicenseFee: number,
+        public retailPrice: number,
     ){}
 }
